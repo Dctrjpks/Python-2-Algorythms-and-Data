20c16a21ec87faff24198ed1aab3d7f4b97e86fb
Dctrjpks <47474005+Dctrjpks@users.noreply.github.com>
2019-04-14 22:53:20 +0300

    Update hw1task5.1.py

--- hw1task5.1.py
+++ hw1task5.1.py
@@ -1,13 +1,11 @@
 # 5. Пользователь вводит две буквы. Определить, на каких местах алфавита
 # они стоят  и сколько между ними находится букв.
 import string
-# letters_nums = []
 eng_alph = 'abcdefghijklmnopqrstuvwxyz'
 rus_alph = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 let_inp_1 = '0'
 while let_inp_1.lower() not in eng_alph and let_inp_1.lower() not in rus_alph:
     let_inp_1 = input('Введите первую букву :')
-# print (ord(let_inp_1))
 let_inp_2 = '0'
 if let_inp_1.lower() in eng_alph:
     while let_inp_2.lower() not in eng_alph:
@@ -16,7 +14,6 @@ if let_inp_1.lower() in eng_alph:
     let2 = let_inp_2.lower()
     let_place_1 = eng_alph.find(let1) + 1
     let_place_2 = eng_alph.find(let2) + 1
-
     print('Номер буквы "{}" в английском алфавите - {}.'.format(let1, let_place_1))
     print('Номер буквы "{}" в английском алфавите - {}.'.format(let2, let_place_2))
     print('Между ними {} букв.'.format(abs(let_place_2 - let_place_1) - 1))
